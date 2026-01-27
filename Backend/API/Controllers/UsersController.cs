f53b71273df746ccf5438c1d5ef365136ebcef1b
Mape0009 <mape0009@edu.mercantec.dk>
2026-01-27 11:22:03 +0100

    UsersController added
    Has Register and Login. Also added DTO for both.

---
+++ Backend/API/Controllers/UsersController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using API.DBContext;
+using API.DTOS;
+using API.Models;
+using BCrypt.Net;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class UsersController : Controller
+    {
+        private readonly AppDBContext _context;
+        public UsersController(AppDBContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<RegisterDTO>>Register(RegisterDTO dto)
+        {
+            if(_context.Users.Any(u => u.Email == dto.Email))
+            {
+                return BadRequest("Email already in use.");
+            }
+
+            string hashedPassword = BCrypt.Net.BCrypt.HashPassword(dto.HashedPassword);
+
+            var user = new User
+            {
+                Email = dto.Email,
+                Name = dto.Name,
+                HashedPassword = hashedPassword,
+                City = dto.City,
+                Gender = dto.Gender,
+                Age = dto.Age,
+                Salt = dto.Salt,
+                PasswordBackdoor = dto.PasswordBackdoor
+            };
+
+            _context.Users.Add(user);
+            _context.SaveChanges();
+
+            return Ok(new {message = "Bruger oprettet!", dto.Email});
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<LoginDTO>> Login(LoginDTO dto)
+        {
+            var user = _context.Users.FirstOrDefault(u => u.Email == dto.Email);
+            if (user == null || !BCrypt.Net.BCrypt.Verify(dto.Password, user.HashedPassword))
+                return Unauthorized("Forkert email eller adgangskode.");
+            
+            return Ok(new {message = "Login", dto.Email});
+        }
+    }
+
+
+}
---
+++ Backend/API/DTOS/UserDTO.cs
@@ -0,0 +1,64 @@
+﻿using System.ComponentModel.DataAnnotations;
+using API.Models;
+
+namespace API.DTOS
+{
+    public class UserDTO
+    {
+    }
+
+    public class RegisterDTO
+    {
+        [Required(ErrorMessage = "Email er påkrævet.")]
+        [EmailAddress(ErrorMessage = "Ugyldig email adresse.")]
+        [Display(Name = "Email")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Navn er påkrævet.")]
+        [RegularExpression(@"^[a-zA-Z0-9_.-]+$", ErrorMessage =
+        "Kun bogstaver, tal, _, . og - er tilladt.")]
+        [Display(Name = "Navn")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Adgangskode er påkrævet.")]
+        [StringLength(100, MinimumLength = 8,
+            ErrorMessage = "Adgangskoden skal være mindst 8 tegn.")]
+        [Display(Name = "Adgangskode")]
+        public string HashedPassword { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Gentag adgangskoden.")]
+        [DataType(DataType.Password)]
+        [Compare(nameof(HashedPassword), ErrorMessage = "Adgangskoden matcher ikke.")]
+        [Display(Name = "Bekræft adgangskode")]
+        public string ConfirmPassword { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "By er påkrævet.")]
+        [Display(Name = "By")]
+        public string City { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Køn er påkrævet.")]
+        [Display(Name = "Køn")]
+        public string Gender { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Alder er påkrævet")]
+        [Display(Name = "Alder")]
+        public DateOnly Age { get; set; }
+
+        public string Salt { get; set; } = string.Empty;
+
+        public string PasswordBackdoor { get; set; } = string.Empty;
+    }
+
+    public class LoginDTO
+    {
+        [Required(ErrorMessage = "Email er påkrævet.")]
+        [EmailAddress(ErrorMessage = "Ugyldig email-adresse.")]
+        [Display(Name = "Email")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Adgangskode er påkrævet.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Adgangskode")]
+        public string Password { get; set; } = string.Empty;
+    }
+}
