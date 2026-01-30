1f685635bd19b038541d1f3bcf68914f832fe271
Elic0 <charlieoljac@gmail.com>
2026-01-30 08:25:35 +0100

    changed base url and added default .dart files
    changed the api base url to be the correct one (venneplan-api) and created default files for chat/group

--- flutter_app/lib/core/constants/api_constants.dart
+++ flutter_app/lib/core/constants/api_constants.dart
@@ -1,10 +1,12 @@
 class ApiConstants {
   // Backend API URL 
-  static const String baseUrl = 'https://h4-api.mercantec.tech/api';
+  static const String baseUrl = 'https://venneplan-api.mercantec.tech/api';
   
   // Weather endpoints
   static const String weatherForecast = '/WeatherForecast';
+  static const String chats = '/Chats';
   
   // Full URLs
   static String get weatherForecastUrl => '$baseUrl$weatherForecast';
+  static String get chatsUrl => '$baseUrl$chats';
 } 
